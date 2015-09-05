44b93bce24b12efb9d3b8a3a9d07f4b3aee30ee3
jun.jiang <1669266647@qq.com>
2015-09-05 12:24:18 +0800

    add resize feature

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -29,6 +29,22 @@
         private void InitializeComponent()
         {
             this.panelTop = new System.Windows.Forms.Panel();
+            this.pictureBox_TopEdge = new System.Windows.Forms.PictureBox();
+            this.pictureBox_LeftEdge = new System.Windows.Forms.PictureBox();
+            this.pictureBox_BottomEdge = new System.Windows.Forms.PictureBox();
+            this.pictureBox_RightEdge = new System.Windows.Forms.PictureBox();
+            this.pictureBox_LeftTopCorner = new System.Windows.Forms.PictureBox();
+            this.pictureBox_LeftBottomCorner = new System.Windows.Forms.PictureBox();
+            this.pictureBox_RightBottomCorner = new System.Windows.Forms.PictureBox();
+            this.pictureBox_RightTopCorner = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_TopEdge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftEdge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BottomEdge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightEdge)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftTopCorner)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftBottomCorner)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightBottomCorner)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightTopCorner)).BeginInit();
             this.SuspendLayout();
             // 
             // panelTop
@@ -42,6 +58,110 @@
             this.panelTop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panelTop_MouseMove);
             this.panelTop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panelTop_MouseUp);
             // 
+            // pictureBox_TopEdge
+            // 
+            this.pictureBox_TopEdge.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox_TopEdge.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.pictureBox_TopEdge.Location = new System.Drawing.Point(1, 1);
+            this.pictureBox_TopEdge.Name = "pictureBox_TopEdge";
+            this.pictureBox_TopEdge.Size = new System.Drawing.Size(798, 1);
+            this.pictureBox_TopEdge.TabIndex = 1;
+            this.pictureBox_TopEdge.TabStop = false;
+            this.pictureBox_TopEdge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_TopEdge_MouseDown);
+            this.pictureBox_TopEdge.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_TopEdge_MouseMove);
+            this.pictureBox_TopEdge.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_TopEdge_MouseUp);
+            // 
+            // pictureBox_LeftEdge
+            // 
+            this.pictureBox_LeftEdge.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox_LeftEdge.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pictureBox_LeftEdge.Location = new System.Drawing.Point(1, 2);
+            this.pictureBox_LeftEdge.Name = "pictureBox_LeftEdge";
+            this.pictureBox_LeftEdge.Size = new System.Drawing.Size(1, 596);
+            this.pictureBox_LeftEdge.TabIndex = 2;
+            this.pictureBox_LeftEdge.TabStop = false;
+            this.pictureBox_LeftEdge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftEdge_MouseDown);
+            this.pictureBox_LeftEdge.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftEdge_MouseMove);
+            this.pictureBox_LeftEdge.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftEdge_MouseUp);
+            // 
+            // pictureBox_BottomEdge
+            // 
+            this.pictureBox_BottomEdge.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox_BottomEdge.Cursor = System.Windows.Forms.Cursors.SizeNS;
+            this.pictureBox_BottomEdge.Location = new System.Drawing.Point(1, 598);
+            this.pictureBox_BottomEdge.Name = "pictureBox_BottomEdge";
+            this.pictureBox_BottomEdge.Size = new System.Drawing.Size(798, 1);
+            this.pictureBox_BottomEdge.TabIndex = 3;
+            this.pictureBox_BottomEdge.TabStop = false;
+            this.pictureBox_BottomEdge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_BottomEdge_MouseDown);
+            this.pictureBox_BottomEdge.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_BottomEdge_MouseMove);
+            this.pictureBox_BottomEdge.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_BottomEdge_MouseUp);
+            // 
+            // pictureBox_RightEdge
+            // 
+            this.pictureBox_RightEdge.BackColor = System.Drawing.Color.Gray;
+            this.pictureBox_RightEdge.Cursor = System.Windows.Forms.Cursors.SizeWE;
+            this.pictureBox_RightEdge.Location = new System.Drawing.Point(798, 2);
+            this.pictureBox_RightEdge.Name = "pictureBox_RightEdge";
+            this.pictureBox_RightEdge.Size = new System.Drawing.Size(1, 596);
+            this.pictureBox_RightEdge.TabIndex = 4;
+            this.pictureBox_RightEdge.TabStop = false;
+            this.pictureBox_RightEdge.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightEdge_MouseDown);
+            this.pictureBox_RightEdge.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightEdge_MouseMove);
+            this.pictureBox_RightEdge.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightEdge_MouseUp);
+            // 
+            // pictureBox_LeftTopCorner
+            // 
+            this.pictureBox_LeftTopCorner.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_LeftTopCorner.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pictureBox_LeftTopCorner.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox_LeftTopCorner.Name = "pictureBox_LeftTopCorner";
+            this.pictureBox_LeftTopCorner.Size = new System.Drawing.Size(5, 5);
+            this.pictureBox_LeftTopCorner.TabIndex = 13;
+            this.pictureBox_LeftTopCorner.TabStop = false;
+            this.pictureBox_LeftTopCorner.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftTopCorner_MouseDown);
+            this.pictureBox_LeftTopCorner.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftTopCorner_MouseMove);
+            this.pictureBox_LeftTopCorner.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftTopCorner_MouseUp);
+            // 
+            // pictureBox_LeftBottomCorner
+            // 
+            this.pictureBox_LeftBottomCorner.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_LeftBottomCorner.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.pictureBox_LeftBottomCorner.Location = new System.Drawing.Point(0, 595);
+            this.pictureBox_LeftBottomCorner.Name = "pictureBox_LeftBottomCorner";
+            this.pictureBox_LeftBottomCorner.Size = new System.Drawing.Size(5, 5);
+            this.pictureBox_LeftBottomCorner.TabIndex = 14;
+            this.pictureBox_LeftBottomCorner.TabStop = false;
+            this.pictureBox_LeftBottomCorner.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftBottomCorner_MouseDown);
+            this.pictureBox_LeftBottomCorner.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftBottomCorner_MouseMove);
+            this.pictureBox_LeftBottomCorner.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_LeftBottomCorner_MouseUp);
+            // 
+            // pictureBox_RightBottomCorner
+            // 
+            this.pictureBox_RightBottomCorner.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_RightBottomCorner.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.pictureBox_RightBottomCorner.Location = new System.Drawing.Point(795, 595);
+            this.pictureBox_RightBottomCorner.Name = "pictureBox_RightBottomCorner";
+            this.pictureBox_RightBottomCorner.Size = new System.Drawing.Size(5, 5);
+            this.pictureBox_RightBottomCorner.TabIndex = 15;
+            this.pictureBox_RightBottomCorner.TabStop = false;
+            this.pictureBox_RightBottomCorner.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightBottomCorner_MouseDown);
+            this.pictureBox_RightBottomCorner.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightBottomCorner_MouseMove);
+            this.pictureBox_RightBottomCorner.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightBottomCorner_MouseUp);
+            // 
+            // pictureBox_RightTopCorner
+            // 
+            this.pictureBox_RightTopCorner.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox_RightTopCorner.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+            this.pictureBox_RightTopCorner.Location = new System.Drawing.Point(795, 0);
+            this.pictureBox_RightTopCorner.Name = "pictureBox_RightTopCorner";
+            this.pictureBox_RightTopCorner.Size = new System.Drawing.Size(5, 5);
+            this.pictureBox_RightTopCorner.TabIndex = 16;
+            this.pictureBox_RightTopCorner.TabStop = false;
+            this.pictureBox_RightTopCorner.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightTopCorner_MouseDown);
+            this.pictureBox_RightTopCorner.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightTopCorner_MouseMove);
+            this.pictureBox_RightTopCorner.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox_RightTopCorner_MouseUp);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -50,9 +170,26 @@
             this.BackgroundImage = global::RPlayer.Properties.Resources._6;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pictureBox_TopEdge);
+            this.Controls.Add(this.pictureBox_RightEdge);
+            this.Controls.Add(this.pictureBox_LeftEdge);
+            this.Controls.Add(this.pictureBox_BottomEdge);
+            this.Controls.Add(this.pictureBox_RightTopCorner);
+            this.Controls.Add(this.pictureBox_LeftBottomCorner);
+            this.Controls.Add(this.pictureBox_RightBottomCorner);
+            this.Controls.Add(this.pictureBox_LeftTopCorner);
             this.Controls.Add(this.panelTop);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_TopEdge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftEdge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_BottomEdge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightEdge)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftTopCorner)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_LeftBottomCorner)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightBottomCorner)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_RightTopCorner)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -60,6 +197,14 @@
         #endregion
 
         private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.PictureBox pictureBox_TopEdge;
+        private System.Windows.Forms.PictureBox pictureBox_LeftEdge;
+        private System.Windows.Forms.PictureBox pictureBox_BottomEdge;
+        private System.Windows.Forms.PictureBox pictureBox_RightEdge;
+        private System.Windows.Forms.PictureBox pictureBox_LeftTopCorner;
+        private System.Windows.Forms.PictureBox pictureBox_LeftBottomCorner;
+        private System.Windows.Forms.PictureBox pictureBox_RightBottomCorner;
+        private System.Windows.Forms.PictureBox pictureBox_RightTopCorner;
     }
 }
 
--- MainForm.cs
+++ MainForm.cs
@@ -14,6 +14,19 @@ namespace RPlayer
         private bool m_bPanelTopMouseDown = false;
         private Point m_PanelTopMouseDownPos;
 
+        private bool m_bTopEdge_MouseDown = false;
+        private bool m_bLeftEdge_MouseDown = false;
+        private bool m_bBottomEdge_MouseDown = false;
+        private bool m_bRightEdge_MouseDown = false;
+
+        private bool m_bLeftTopCornerMouseDown = false;
+        private bool m_bLeftBottomCornerMouseDown = false;
+        private bool m_bRightBottomCornerMouseDown = false;
+        private bool m_bRightTopCornerMouseDown = false;
+
+        private const int m_nMinSize = 50;
+        private const int m_nEdgeMargin = 1;
+
         public MainForm()
         {
             InitializeComponent();
@@ -38,6 +51,240 @@ namespace RPlayer
         private void panelTop_MouseUp(object sender, MouseEventArgs e)
         {
             m_bPanelTopMouseDown = false;
+        }
+
+        private void pictureBox_TopEdge_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bTopEdge_MouseDown = true;
+        }
+
+        private void pictureBox_TopEdge_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(m_bTopEdge_MouseDown)
+            {
+                Control control = (Control)sender;
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int yDiff = this.Location.Y - MouseScreenPoint.Y;
+                if (this.Size.Height + yDiff > m_nMinSize)
+                {
+                    this.Location = new Point(this.Location.X, MouseScreenPoint.Y);
+                    this.Size = new Size(this.Size.Width, this.Size.Height + yDiff);
+                }  
+            }
+        }
+
+        private void pictureBox_TopEdge_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bTopEdge_MouseDown = false;
+        }
+
+        private void pictureBox_LeftEdge_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bLeftEdge_MouseDown = true;
+        }
+
+        private void pictureBox_LeftEdge_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_bLeftEdge_MouseDown)
+            {
+                Control control = (Control)sender;
+
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = this.Location.X - MouseScreenPoint.X;
+                if (this.Size.Width + xDiff > m_nMinSize)
+                {
+                    this.Location = new Point(MouseScreenPoint.X, this.Location.Y);
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height);
+                }
+            }
+        }
+
+        private void pictureBox_LeftEdge_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bLeftEdge_MouseDown = false;
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            pictureBox_TopEdge.Size
+                = new Size(this.Size.Width - m_nEdgeMargin * 2, 
+                    pictureBox_TopEdge.Size.Height);
+            pictureBox_LeftEdge.Size
+                = new Size(pictureBox_LeftEdge.Width,
+                    this.Size.Height - m_nEdgeMargin * 2 - pictureBox_TopEdge.Size.Height * 2);
+            pictureBox_BottomEdge.Size
+                = new Size(this.Size.Width - m_nEdgeMargin * 2, 
+                    pictureBox_BottomEdge.Size.Height);
+            pictureBox_RightEdge.Size 
+                = new Size(pictureBox_RightEdge.Width,
+                    this.Size.Height - m_nEdgeMargin * 2 - pictureBox_TopEdge.Size.Height * 2);
+
+            pictureBox_BottomEdge.Location
+                = new Point(pictureBox_BottomEdge.Location.X,
+                    this.Size.Height - m_nEdgeMargin - pictureBox_BottomEdge.Size.Height);
+            pictureBox_RightEdge.Location
+                = new Point(this.Size.Width - m_nEdgeMargin - pictureBox_RightEdge.Size.Width, 
+                    pictureBox_RightEdge.Location.Y);
+            pictureBox_LeftBottomCorner.Location
+                = new Point(pictureBox_LeftBottomCorner.Location.X,
+                this.Size.Height - pictureBox_LeftBottomCorner.Size.Height);
+            pictureBox_RightTopCorner.Location
+                = new Point(this.Size.Width - pictureBox_RightTopCorner.Size.Width,
+                    pictureBox_RightTopCorner.Location.Y);
+            pictureBox_RightBottomCorner.Location
+                = new Point(this.Size.Width - pictureBox_RightBottomCorner.Size.Width,
+                    this.Size.Height - pictureBox_RightBottomCorner.Size.Height);
+        }
+
+        private void pictureBox_BottomEdge_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bBottomEdge_MouseDown = true;
+        }
+
+        private void pictureBox_BottomEdge_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(m_bBottomEdge_MouseDown)
+            {
+                Control control = (Control)sender;
+
+                Point MouseScrrenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                this.Size = new Size(this.Size.Width, MouseScrrenPoint.Y - this.Location.Y);
+            }
+        }
+
+        private void pictureBox_BottomEdge_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bBottomEdge_MouseDown = false;
+        }
+
+        private void pictureBox_RightEdge_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bRightEdge_MouseDown = true;
+        }
+
+        private void pictureBox_RightEdge_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(m_bRightEdge_MouseDown)
+            {
+                Control control = (Control)sender;
+
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = MouseScreenPoint.X - (this.Location.X + this.Size.Width);
+                if (this.Size.Width + xDiff > m_nMinSize)
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height);
+            }
+        }
+
+        private void pictureBox_RightEdge_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bRightEdge_MouseDown = false;
+        }
+
+        private void pictureBox_RightBottomCorner_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bRightBottomCornerMouseDown = true;
+        }
+
+        private void pictureBox_RightBottomCorner_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_bRightBottomCornerMouseDown)
+            {
+                Control control = (Control)sender;
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = MouseScreenPoint.X - (this.Location.X + this.Size.Width);
+                int yDiff = MouseScreenPoint.Y - (this.Location.Y + this.Size.Height);
+                if (this.Size.Width + xDiff > m_nMinSize &&
+                    this.Size.Height + yDiff > m_nMinSize)
+                {
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height + yDiff);
+                }
+            }
+        }
+
+        private void pictureBox_RightBottomCorner_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bRightBottomCornerMouseDown = false;
+        }
+
+        private void pictureBox_LeftTopCorner_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bLeftTopCornerMouseDown = true;
+        }
+
+        private void pictureBox_LeftTopCorner_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_bLeftTopCornerMouseDown)
+            {
+                Control control = (Control)sender;
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = this.Location.X - MouseScreenPoint.X;
+                int yDiff = this.Location.Y - MouseScreenPoint.Y;
+                if (this.Size.Width + xDiff > m_nMinSize &&
+                    this.Size.Height + yDiff > m_nMinSize)
+                {
+                    this.Location = new Point(MouseScreenPoint.X, MouseScreenPoint.Y);
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height + yDiff);
+                }
+            }
+        }
+
+        private void pictureBox_LeftTopCorner_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bLeftTopCornerMouseDown = false;
+        }
+
+        private void pictureBox_LeftBottomCorner_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bLeftBottomCornerMouseDown = true;
+        }
+
+        private void pictureBox_LeftBottomCorner_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_bLeftBottomCornerMouseDown)
+            {
+                Control control = (Control)sender;
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = this.Location.X - MouseScreenPoint.X;
+                int yDiff = MouseScreenPoint.Y - (this.Location.Y + this.Size.Height);
+                if (this.Size.Width + xDiff > m_nMinSize &&
+                    this.Size.Height + yDiff > m_nMinSize)
+                {
+                    this.Location = new Point(MouseScreenPoint.X, this.Location.Y);
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height + yDiff);
+                }
+            }
+        }
+
+        private void pictureBox_LeftBottomCorner_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bLeftBottomCornerMouseDown = false;
+        }
+
+        private void pictureBox_RightTopCorner_MouseDown(object sender, MouseEventArgs e)
+        {
+            m_bRightTopCornerMouseDown = true;
+        }
+
+        private void pictureBox_RightTopCorner_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (m_bRightTopCornerMouseDown)
+            {
+                Control control = (Control)sender;
+                Point MouseScreenPoint = control.PointToScreen(new Point(e.X, e.Y));
+                int xDiff = MouseScreenPoint.X - (this.Location.X + this.Size.Width); 
+                int yDiff = this.Location.Y - MouseScreenPoint.Y;
+                if (this.Size.Width + xDiff > m_nMinSize &&
+                    this.Size.Height + yDiff > m_nMinSize)
+                {
+                    this.Location = new Point(this.Location.X, MouseScreenPoint.Y);
+                    this.Size = new Size(this.Size.Width + xDiff, this.Size.Height + yDiff);
+                }
+            }
+        }
+
+        private void pictureBox_RightTopCorner_MouseUp(object sender, MouseEventArgs e)
+        {
+            m_bRightTopCornerMouseDown = false;
         }
     }
 }

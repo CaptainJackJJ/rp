05a30a2449b74b4919d3e9192e8d232b2eca6513
jun.jiang <jun.jiang@vidon.me>
2015-09-26 17:40:46 +0800

    Add FormBottomBar files

---
+++ FormBottomBar.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace RPlayer
+{
+  partial class FormBottomBar
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.colorSlider_volume = new MB.Controls.ColorSlider();
+      this.label_desktop = new System.Windows.Forms.Label();
+      this.label_timeLast = new System.Windows.Forms.Label();
+      this.label_timeCurrent = new System.Windows.Forms.Label();
+      this.colorSlider_playProcess = new MB.Controls.ColorSlider();
+      this.label_Volume = new System.Windows.Forms.Label();
+      this.label_Pre = new System.Windows.Forms.Label();
+      this.label_Next = new System.Windows.Forms.Label();
+      this.label_Stop = new System.Windows.Forms.Label();
+      this.label_Play = new System.Windows.Forms.Label();
+      this.label_FB = new System.Windows.Forms.Label();
+      this.label_FF = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // colorSlider_volume
+      // 
+      this.colorSlider_volume.BackColor = System.Drawing.Color.Transparent;
+      this.colorSlider_volume.BarInnerColor = System.Drawing.Color.Gray;
+      this.colorSlider_volume.BarOuterColor = System.Drawing.Color.Transparent;
+      this.colorSlider_volume.BarPenColor = System.Drawing.Color.Transparent;
+      this.colorSlider_volume.BorderRoundRectSize = new System.Drawing.Size(1, 1);
+      this.colorSlider_volume.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.colorSlider_volume.DrawFocusRectangle = false;
+      this.colorSlider_volume.ElapsedInnerColor = System.Drawing.Color.RoyalBlue;
+      this.colorSlider_volume.ElapsedOuterColor = System.Drawing.Color.MidnightBlue;
+      this.colorSlider_volume.KeyPressRespond = true;
+      this.colorSlider_volume.LargeChange = ((uint)(5u));
+      this.colorSlider_volume.Location = new System.Drawing.Point(763, 35);
+      this.colorSlider_volume.MouseEffects = false;
+      this.colorSlider_volume.MouseWheelBarPartitions = 100;
+      this.colorSlider_volume.MouseWheelRespond = true;
+      this.colorSlider_volume.Name = "colorSlider_volume";
+      this.colorSlider_volume.Size = new System.Drawing.Size(100, 10);
+      this.colorSlider_volume.SmallChange = ((uint)(1u));
+      this.colorSlider_volume.TabIndex = 42;
+      this.colorSlider_volume.ThumbInnerColor = System.Drawing.Color.White;
+      this.colorSlider_volume.ThumbPenColor = System.Drawing.Color.Transparent;
+      this.colorSlider_volume.ThumbRoundRectSize = new System.Drawing.Size(9, 9);
+      this.colorSlider_volume.ThumbSize = 10;
+      this.colorSlider_volume.MouseEnter += new System.EventHandler(this.label_Volume_MouseEnter);
+      this.colorSlider_volume.MouseLeave += new System.EventHandler(this.label_Volume_MouseLeave);
+      // 
+      // label_desktop
+      // 
+      this.label_desktop.BackColor = System.Drawing.Color.Transparent;
+      this.label_desktop.Location = new System.Drawing.Point(871, 37);
+      this.label_desktop.Name = "label_desktop";
+      this.label_desktop.Size = new System.Drawing.Size(25, 25);
+      this.label_desktop.TabIndex = 41;
+      this.label_desktop.Click += new System.EventHandler(this.label_desktop_Click);
+      this.label_desktop.MouseEnter += new System.EventHandler(this.label_desktop_MouseEnter);
+      this.label_desktop.MouseLeave += new System.EventHandler(this.label_desktop_MouseLeave);
+      // 
+      // label_timeLast
+      // 
+      this.label_timeLast.AutoSize = true;
+      this.label_timeLast.BackColor = System.Drawing.Color.Transparent;
+      this.label_timeLast.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+      this.label_timeLast.ForeColor = System.Drawing.Color.White;
+      this.label_timeLast.Location = new System.Drawing.Point(811, 3);
+      this.label_timeLast.Name = "label_timeLast";
+      this.label_timeLast.Size = new System.Drawing.Size(77, 17);
+      this.label_timeLast.TabIndex = 40;
+      this.label_timeLast.Text = "-01 : 00 : 30";
+      this.label_timeLast.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // label_timeCurrent
+      // 
+      this.label_timeCurrent.AutoSize = true;
+      this.label_timeCurrent.BackColor = System.Drawing.Color.Transparent;
+      this.label_timeCurrent.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+      this.label_timeCurrent.ForeColor = System.Drawing.Color.White;
+      this.label_timeCurrent.Location = new System.Drawing.Point(18, 3);
+      this.label_timeCurrent.Name = "label_timeCurrent";
+      this.label_timeCurrent.Size = new System.Drawing.Size(72, 17);
+      this.label_timeCurrent.TabIndex = 39;
+      this.label_timeCurrent.Text = "00 : 00 : 00";
+      this.label_timeCurrent.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      // 
+      // colorSlider_playProcess
+      // 
+      this.colorSlider_playProcess.BackColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.BarInnerColor = System.Drawing.Color.Gray;
+      this.colorSlider_playProcess.BarOuterColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.BarPenColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.BorderRoundRectSize = new System.Drawing.Size(1, 1);
+      this.colorSlider_playProcess.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.colorSlider_playProcess.DrawFocusRectangle = false;
+      this.colorSlider_playProcess.ElapsedInnerColor = System.Drawing.Color.RoyalBlue;
+      this.colorSlider_playProcess.ElapsedOuterColor = System.Drawing.Color.MidnightBlue;
+      this.colorSlider_playProcess.LargeChange = ((uint)(5u));
+      this.colorSlider_playProcess.Location = new System.Drawing.Point(93, 6);
+      this.colorSlider_playProcess.MouseEffects = false;
+      this.colorSlider_playProcess.MouseWheelBarPartitions = 100;
+      this.colorSlider_playProcess.Name = "colorSlider_playProcess";
+      this.colorSlider_playProcess.Size = new System.Drawing.Size(715, 10);
+      this.colorSlider_playProcess.SmallChange = ((uint)(1u));
+      this.colorSlider_playProcess.TabIndex = 38;
+      this.colorSlider_playProcess.ThumbInnerColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.ThumbOuterColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.ThumbPenColor = System.Drawing.Color.Transparent;
+      this.colorSlider_playProcess.ThumbRoundRectSize = new System.Drawing.Size(9, 9);
+      this.colorSlider_playProcess.ThumbSize = 9;
+      this.colorSlider_playProcess.MouseEnter += new System.EventHandler(this.colorSlider_playProcess_MouseEnter);
+      this.colorSlider_playProcess.MouseLeave += new System.EventHandler(this.colorSlider_playProcess_MouseLeave);
+      // 
+      // label_Volume
+      // 
+      this.label_Volume.BackColor = System.Drawing.Color.Transparent;
+      this.label_Volume.Location = new System.Drawing.Point(734, 28);
+      this.label_Volume.Name = "label_Volume";
+      this.label_Volume.Size = new System.Drawing.Size(25, 25);
+      this.label_Volume.TabIndex = 47;
+      this.label_Volume.MouseEnter += new System.EventHandler(this.label_Volume_MouseEnter);
+      this.label_Volume.MouseLeave += new System.EventHandler(this.label_Volume_MouseLeave);
+      // 
+      // label_Pre
+      // 
+      this.label_Pre.BackColor = System.Drawing.Color.Transparent;
+      this.label_Pre.Location = new System.Drawing.Point(346, 28);
+      this.label_Pre.Name = "label_Pre";
+      this.label_Pre.Size = new System.Drawing.Size(25, 25);
+      this.label_Pre.TabIndex = 46;
+      this.label_Pre.MouseEnter += new System.EventHandler(this.label_Pre_MouseEnter);
+      this.label_Pre.MouseLeave += new System.EventHandler(this.label_Pre_MouseLeave);
+      // 
+      // label_Next
+      // 
+      this.label_Next.BackColor = System.Drawing.Color.Transparent;
+      this.label_Next.Location = new System.Drawing.Point(521, 28);
+      this.label_Next.Name = "label_Next";
+      this.label_Next.Size = new System.Drawing.Size(25, 25);
+      this.label_Next.TabIndex = 45;
+      this.label_Next.MouseEnter += new System.EventHandler(this.label_Next_MouseEnter);
+      this.label_Next.MouseLeave += new System.EventHandler(this.label_Next_MouseLeave);
+      // 
+      // label_Stop
+      // 
+      this.label_Stop.BackColor = System.Drawing.Color.Transparent;
+      this.label_Stop.Location = new System.Drawing.Point(386, 28);
+      this.label_Stop.Name = "label_Stop";
+      this.label_Stop.Size = new System.Drawing.Size(25, 25);
+      this.label_Stop.TabIndex = 44;
+      this.label_Stop.MouseEnter += new System.EventHandler(this.label_Stop_MouseEnter);
+      this.label_Stop.MouseLeave += new System.EventHandler(this.label_Stop_MouseLeave);
+      // 
+      // label_Play
+      // 
+      this.label_Play.BackColor = System.Drawing.Color.Transparent;
+      this.label_Play.Location = new System.Drawing.Point(426, 22);
+      this.label_Play.Name = "label_Play";
+      this.label_Play.Size = new System.Drawing.Size(40, 40);
+      this.label_Play.TabIndex = 43;
+      this.label_Play.Click += new System.EventHandler(this.label_Play_Click);
+      this.label_Play.MouseEnter += new System.EventHandler(this.label_Play_MouseEnter);
+      this.label_Play.MouseLeave += new System.EventHandler(this.label_Play_MouseLeave);
+      // 
+      // label_FB
+      // 
+      this.label_FB.BackColor = System.Drawing.Color.Transparent;
+      this.label_FB.Location = new System.Drawing.Point(484, 28);
+      this.label_FB.Name = "label_FB";
+      this.label_FB.Size = new System.Drawing.Size(25, 25);
+      this.label_FB.TabIndex = 48;
+      this.label_FB.Click += new System.EventHandler(this.label_FB_Click);
+      this.label_FB.MouseEnter += new System.EventHandler(this.label_FB_MouseEnter);
+      this.label_FB.MouseLeave += new System.EventHandler(this.label_FB_MouseLeave);
+      // 
+      // label_FF
+      // 
+      this.label_FF.BackColor = System.Drawing.Color.Transparent;
+      this.label_FF.Location = new System.Drawing.Point(515, 31);
+      this.label_FF.Name = "label_FF";
+      this.label_FF.Size = new System.Drawing.Size(25, 25);
+      this.label_FF.TabIndex = 49;
+      this.label_FF.MouseEnter += new System.EventHandler(this.label_FF_MouseEnter);
+      this.label_FF.MouseLeave += new System.EventHandler(this.label_FF_MouseLeave);
+      // 
+      // FormBottomBar
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.Black;
+      this.ClientSize = new System.Drawing.Size(915, 65);
+      this.Controls.Add(this.label_FF);
+      this.Controls.Add(this.label_FB);
+      this.Controls.Add(this.label_Volume);
+      this.Controls.Add(this.label_Pre);
+      this.Controls.Add(this.label_Next);
+      this.Controls.Add(this.label_Stop);
+      this.Controls.Add(this.label_Play);
+      this.Controls.Add(this.colorSlider_volume);
+      this.Controls.Add(this.label_desktop);
+      this.Controls.Add(this.label_timeLast);
+      this.Controls.Add(this.label_timeCurrent);
+      this.Controls.Add(this.colorSlider_playProcess);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "FormBottomBar";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+      this.Resize += new System.EventHandler(this.FormBottomBar_Resize);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private MB.Controls.ColorSlider colorSlider_volume;
+    private System.Windows.Forms.Label label_desktop;
+    private System.Windows.Forms.Label label_timeLast;
+    private System.Windows.Forms.Label label_timeCurrent;
+    private MB.Controls.ColorSlider colorSlider_playProcess;
+    private System.Windows.Forms.Label label_Volume;
+    private System.Windows.Forms.Label label_Pre;
+    private System.Windows.Forms.Label label_Next;
+    private System.Windows.Forms.Label label_Stop;
+    private System.Windows.Forms.Label label_Play;
+    private System.Windows.Forms.Label label_FB;
+    private System.Windows.Forms.Label label_FF;
+  }
+}
---
+++ FormBottomBar.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using RpCoreWrapper;
+
+namespace RPlayer
+{
+  public partial class FormBottomBar : Form
+  {
+    private const int m_nPlayButtonSize = 40;
+    private const int m_nBottomButtonsSize = 25;
+    private const int m_nBottomButtonsMargin = 15;
+    private const int m_nBottomBtnsToPlayBtnYMargin = (int)((m_nPlayButtonSize - m_nBottomButtonsSize) * 0.5);
+    private const int m_nPlayProcessToPlayBtnYMargin = 5;
+    private const int m_nPlayProcessXMargin = 90;
+
+
+    private const int m_nStopBtnXMarginToPlay = -(m_nBottomButtonsMargin * 3 + m_nBottomButtonsSize * 3);
+    private const int m_nFBBtnXMarginToPlay = -(m_nBottomButtonsMargin + m_nBottomButtonsSize);
+    private const int m_nPreBtnXMarginToPlay = -(m_nBottomButtonsMargin * 2 + m_nBottomButtonsSize * 2);
+    private const int m_nFFBtnXMarginToPlay = m_nPlayButtonSize + m_nBottomButtonsMargin;
+    private const int m_nNextBtnXMarginToPlay = m_nPlayButtonSize + m_nBottomButtonsMargin * 2 + m_nBottomButtonsSize;
+
+    private bool m_bDesktop;
+    private bool m_bMute = false;
+
+    public FormBottomBar()
+    {
+      InitializeComponent();
+
+      try
+      {
+        label_Play.Image = Image.FromFile(Application.StartupPath + @"\pic\play.png");
+        label_Stop.Image = Image.FromFile(Application.StartupPath + @"\pic\stop.png");
+        label_FF.Image = Image.FromFile(Application.StartupPath + @"\pic\FF.png");
+        label_FB.Image = Image.FromFile(Application.StartupPath + @"\pic\FB.png");
+        label_Next.Image = Image.FromFile(Application.StartupPath + @"\pic\Next.png");
+        label_Pre.Image = Image.FromFile(Application.StartupPath + @"\pic\pre.png");
+        label_desktop.Image = Image.FromFile(Application.StartupPath + @"\pic\desktop.png");
+        label_Volume.Image = Image.FromFile(Application.StartupPath + @"\pic\Volume.png");
+      }
+      catch
+      {
+        this.BackColor = Color.Gainsboro;
+        label_Play.Text = "play";
+        label_Stop.Text = "stop";
+        label_FF.Text = "ff";
+        label_FB.Text = "fb";
+        label_Next.Text = "next";
+        label_desktop.Text = "desktop";
+      }
+    }
+
+    private void FormBottomBar_Resize(object sender, EventArgs e)
+    {
+      label_Play.Location =
+   new Point(((int)(this.Size.Width * 0.5) - (int)(label_Play.Size.Width * 0.5)),
+        this.Size.Height - 50);
+      int nBottomButtonsY = label_Play.Location.Y + m_nBottomBtnsToPlayBtnYMargin;
+      label_Stop.Location =
+         new Point((label_Play.Location.X + m_nStopBtnXMarginToPlay),
+              nBottomButtonsY);
+      label_FF.Location =
+         new Point((label_Play.Location.X + m_nFFBtnXMarginToPlay),
+              nBottomButtonsY);
+      label_FB.Location =
+         new Point((label_Play.Location.X + m_nFBBtnXMarginToPlay),
+              nBottomButtonsY);
+      label_Next.Location =
+         new Point((label_Play.Location.X + m_nNextBtnXMarginToPlay),
+              nBottomButtonsY);
+      label_Pre.Location =
+         new Point((label_Play.Location.X + m_nPreBtnXMarginToPlay),
+              nBottomButtonsY);
+      label_desktop.Location =
+        new Point(this.Width - m_nBottomButtonsSize, nBottomButtonsY);
+      colorSlider_volume.Location =
+        new Point(label_desktop.Location.X - 10 - colorSlider_volume.Width, nBottomButtonsY + 7);
+      label_Volume.Location =
+        new Point(colorSlider_volume.Location.X - label_Volume.Width, nBottomButtonsY);
+
+      int nPlayProcessY = label_Play.Location.Y - m_nPlayProcessToPlayBtnYMargin - colorSlider_playProcess.Height;
+
+      label_timeCurrent.Location =
+    new Point(m_nPlayProcessXMargin - label_timeCurrent.Width, nPlayProcessY - 4);
+      label_timeLast.Location =
+          new Point(m_nPlayProcessXMargin + colorSlider_playProcess.Width, nPlayProcessY - 4);
+
+      colorSlider_playProcess.Size
+    = new Size(this.Width - (m_nPlayProcessXMargin * 2), colorSlider_playProcess.Height);
+    }
+
+    private void label_Play_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Play.Image = Image.FromFile(Application.StartupPath + @"\pic\playFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_Play_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Play.Image = Image.FromFile(Application.StartupPath + @"\pic\play.png");
+      }
+      catch { }
+    }
+
+    private void label_Stop_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Stop.Image = Image.FromFile(Application.StartupPath + @"\pic\stopFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_Stop_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Stop.Image = Image.FromFile(Application.StartupPath + @"\pic\stop.png");
+      }
+      catch { }
+    }
+
+    private void label_FF_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_FF.Image = Image.FromFile(Application.StartupPath + @"\pic\FFFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_FF_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_FF.Image = Image.FromFile(Application.StartupPath + @"\pic\FF.png");
+      }
+      catch { }
+    }
+
+    private void label_FB_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_FB.Image = Image.FromFile(Application.StartupPath + @"\pic\FBFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_FB_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_FB.Image = Image.FromFile(Application.StartupPath + @"\pic\FB.png");
+      }
+      catch { }
+    }
+
+    private void label_Next_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Next.Image = Image.FromFile(Application.StartupPath + @"\pic\NextFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_Next_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Next.Image = Image.FromFile(Application.StartupPath + @"\pic\Next.png");
+      }
+      catch { }
+    }
+
+    private void label_Pre_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Pre.Image = Image.FromFile(Application.StartupPath + @"\pic\preFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_Pre_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_Pre.Image = Image.FromFile(Application.StartupPath + @"\pic\pre.png");
+      }
+      catch { }
+    }
+
+    private void colorSlider_playProcess_MouseEnter(object sender, EventArgs e)
+    {
+      colorSlider_playProcess.ThumbInnerColor = Color.White;
+      colorSlider_playProcess.ThumbOuterColor = Color.White;
+    }
+
+    private void colorSlider_playProcess_MouseLeave(object sender, EventArgs e)
+    {
+      colorSlider_playProcess.ThumbInnerColor = Color.Transparent;
+      colorSlider_playProcess.ThumbOuterColor = Color.Transparent;
+    }
+
+    private void label_desktop_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        label_desktop.Image = Image.FromFile(Application.StartupPath + @"\pic\desktopFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_desktop_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        label_desktop.Image = Image.FromFile(Application.StartupPath + @"\pic\desktop.png");
+      }
+      catch { }
+    }
+
+    private void label_Volume_MouseEnter(object sender, EventArgs e)
+    {
+      try
+      {
+        if (m_bMute)
+          label_Volume.Image = Image.FromFile(Application.StartupPath + @"\pic\VolumeMuteFocus.png");
+        else
+          label_Volume.Image = Image.FromFile(Application.StartupPath + @"\pic\VolumeFocus.png");
+      }
+      catch { }
+    }
+
+    private void label_Volume_MouseLeave(object sender, EventArgs e)
+    {
+      try
+      {
+        if (m_bMute)
+          label_Volume.Image = Image.FromFile(Application.StartupPath + @"\pic\VolumeMute.png");
+        else
+          label_Volume.Image = Image.FromFile(Application.StartupPath + @"\pic\Volume.png");
+      }
+      catch { }
+    }
+
+    private void label_FB_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private void label_Play_Click(object sender, EventArgs e)
+    {
+      RpCore.Play("F:\\av\\FileSource\\AVATAR.Title1.mp4", 0);
+    }
+
+    private void label_desktop_Click(object sender, EventArgs e)
+    {
+      //if (m_bDesktop)
+      //{
+      //  m_bDesktop = false;
+      //  this.WindowState = FormWindowState.Normal;
+      //  label_playWnd.Location = new Point(0, label_Close.Size.Height * 3);
+      //}
+      //else
+      //{
+      //  m_bDesktop = true;
+      //  this.WindowState = FormWindowState.Maximized;
+      //  label_playWnd.Location = this.Location;
+      //  label_playWnd.Size = this.Size;
+      //}
+    }
+  }
+}

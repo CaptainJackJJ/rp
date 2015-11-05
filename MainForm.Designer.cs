43bd927a5715f5a91d82b4325ba4f22e475fa3db
jun.jiang <1669266647@qq.com>
2015-11-05 23:46:13 +0800

    Use title mode for background

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -239,7 +239,6 @@
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
       this.BackColor = System.Drawing.Color.Black;
-      this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
       this.ClientSize = new System.Drawing.Size(915, 562);
       this.Controls.Add(this.label_openFile);
       this.Controls.Add(this.label_playWnd);

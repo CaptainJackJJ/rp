5646836a08bfdde03c6e162b9d3d40c6fcd69871
jun.jiang <1669266647@qq.com>
2015-11-22 10:39:57 +0800

    Version 1.0.1

--- MainForm.cs
+++ MainForm.cs
@@ -17,7 +17,7 @@ namespace RPlayer
 {
   public partial class MainForm : Form
   {
-    private readonly string m_strUiVersion = "1.0.0";
+    private readonly string m_strUiVersion = "1.0.1";
     private bool m_bMainFormMouseDown = false;
     private bool m_bTopBarAreaMouseDown = false;
     private Point m_TopBarAreaMouseDownPos;

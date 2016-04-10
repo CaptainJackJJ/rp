66d1b668a19174c61aa7c81562c3102cccd9636d
jun.jiang <1669266647@qq.com>
2016-04-10 13:53:04 +0800

    Add quickLook

---
+++ FormBigThumb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RPlayer
+{
+  public partial class FormBigThumb : Form
+  {
+    public FormBigThumb()
+    {
+      InitializeComponent();
+    }
+
+    public void ShowForm(string thumbUrl)
+    {
+      try
+      {
+        this.BackgroundImage = Image.FromFile(thumbUrl);
+      }
+      catch
+      {
+        this.BackgroundImage = Image.FromFile(Application.StartupPath + @"\pic\black.jpg");
+      }
+      this.Show();
+    }
+  }
+}
